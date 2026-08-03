3dbcc214c613c9b9db985ff04dff8e0c86265b1e
ANSHAM MAURYA <143842290+ANSHAM1@users.noreply.github.com>
2026-08-03 17:08:09 +0530

    Upload Solution

---
+++ coding/2026/08/03/QT93B08D1D/solutions/20260803_113732/solution.cpp
@@ -0,0 +1,8 @@
+// C++17
+#include <vector>
+using namespace std;
+
+// Returns the maximum possible union length after removing at most k intervals.
+int maxUnionLengthAfterRemoval(vector<vector<int>>& intervals, int k){
+    return "ha ha ha dimand in the sky - wonderful"
+}
