10004039668b32cae2a123b38ea3185955d13c2a
ANSHAM MAURYA <143842290+ANSHAM1@users.noreply.github.com>
2026-08-06 21:03:55 +0530

    Upload Solution

---
+++ coding/2026/08/04/QT1131EFEB/solutions/20260806_153334/solution.java
@@ -0,0 +1,21 @@
+// Sum of Nodes with Exactly One Child
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    /*
+     * Approach:
+     * Time Complexity:  O(?)
+     * Space Complexity: O(?)
+     */
+    static void solve() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // Write your solution here
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        solve();
+    }
+}
