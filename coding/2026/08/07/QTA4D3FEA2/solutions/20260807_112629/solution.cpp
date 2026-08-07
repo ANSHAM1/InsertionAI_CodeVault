2f076d0004b2f19f1e30b02176e748cf9e871bbe
ANSHAM MAURYA <143842290+ANSHAM1@users.noreply.github.com>
2026-08-07 16:59:00 +0530

    Upload Solution

---
+++ coding/2026/08/07/QTA4D3FEA2/solutions/20260807_112629/solution.cpp
@@ -0,0 +1,16 @@
+// Design a Log Parser for Hierarchical System Events
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+ * Approach:
+ * Time Complexity:  O(?)
+ * Space Complexity: O(?)
+ */
+class LogParser {
+public:
+    void addLog() {
+        // Write your solution here
+
+    }
+};
