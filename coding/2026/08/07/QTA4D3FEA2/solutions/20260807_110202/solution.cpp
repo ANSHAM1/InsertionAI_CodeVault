c3ac3658fce3b24fa4c4ae68a9bcd0243165ebef
ANSHAM MAURYA <143842290+ANSHAM1@users.noreply.github.com>
2026-08-07 16:32:36 +0530

    Upload Solution

---
+++ coding/2026/08/07/QTA4D3FEA2/solutions/20260807_110202/solution.cpp
@@ -0,0 +1,26 @@
+// Design a Log Parser for Hierarchical System Events
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+ * Approach:
+ * Time Complexity:  O(?)
+ * Space Complexity: O(?)
+ */
+class Solution {
+public:
+    void solve() {
+        // Write your solution here
+
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    Solution sol;
+    sol.solve();
+
+    return 0;
+}
