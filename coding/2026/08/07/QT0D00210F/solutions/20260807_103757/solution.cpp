7e6d427ec021e6f3e9f2941aaebff6f084e52d99
ANSHAM MAURYA <143842290+ANSHAM1@users.noreply.github.com>
2026-08-07 16:08:49 +0530

    Upload Solution

---
+++ coding/2026/08/07/QT0D00210F/solutions/20260807_103757/solution.cpp
@@ -0,0 +1,26 @@
+// Find the Longest Subarray with Equal Number of 0s and 1s
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+ * Approach:
+ * Time Complexity:  O(?)
+ * Space Complexity: O(?)
+ */
+class Solution {
+public:
+    void solve() {
+        // Write your solution here
+
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    Solution sol;
+    sol.solve();
+
+    return 0;
+}
